a360ca8c1d4e7e13d07a87a0fe43bc3e9e47c423
Dung <ledinhdung2908@gmail.com>
2022-12-17 09:24:55 +0700

    PC

--- DeThiMau/DeThiMau/js/script.js
+++ DeThiMau/DeThiMau/js/script.js
@@ -1,26 +1,29 @@
 // coding implementation
 
-const regexname = /^[a-z A-Z]+$/
-const regexemail = /^[a-zA-Z0-9]+@[a-z]+\.[a-z\.]+$/
-const regexphone = /(^\d{10}$)|(^\d{3}-\d{3}-\d{4}$)|(^\d{3} \d{3} \d{4}$)|(^\d{3}\.\d{3}\.\d{4}$)|(^\(\d{3}\) \d{3}-\d{4}$)|(^\+\d{2} \(\d{3}\) \d{3}-\d{4}$)/ 
+// const regexname = /^[a-z A-Z]+$/
+// const regexemail = /^[a-zA-Z0-9]+@[a-z]+\.[a-z\.]+$/
+// const regexphone = /(^\d{10}$)|(^\d{3}-\d{3}-\d{4}$)|(^\d{3} \d{3} \d{4}$)|(^\d{3}\.\d{3}\.\d{4}$)|(^\(\d{3}\) \d{3}-\d{4}$)|(^\+\d{2} \(\d{3}\) \d{3}-\d{4}$)/ 
 // phone
 ///\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/
 
 // password
 //^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$
-const regexpass = /^[a-zA-Z0-9]{6,}$/
-const regexrating = /^[0-9]+$/
+// const regexpass = /^[a-zA-Z0-9]{6,}$/
+// const regexrating = /^[0-9]+$/
+
+
+
 
 
 function validate() {
     var email = document.getElementById('email').value; // kiểm tra email
-    var regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    var regexEmail = /^[a-zA-Z0-9]+@[a-z]+\.[a-z\.]+$/;
 
     var comment = document.getElementById('com').value; // kiểm tra tên
     var regexName = /^[^\d+]*[\d+]{0}[^\d+]*$/;
 
     var rate = document.getElementById('rate').value; // kiểm tra số
-    var regexPhone = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/;
+    //var regexPhone = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/;
 
     if (email == '' && comment == '' && rate == '')
     {
